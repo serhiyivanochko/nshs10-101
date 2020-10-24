15f5b794631535f197dc8bb5d7284d2edad7e5ca
Oleksandr Khomiak <oleksandr.khomiak@gmail.com>
2020-10-24 10:31:58 +0300

    Oleksandr_Khomiak:Added player and block which spawns object if to touch it

--- Oleksandr_Khomiak_Mario/Source.py
+++ Oleksandr_Khomiak_Mario/Source.py
@@ -1,4 +1,29 @@
 from tkinter import*
 win = Tk()
 
+canvas = Canvas(width=1000, height=600)
+canvas.pack()
+
+player = canvas.create_rectangle(500, 500, 550, 550, fill="red")
+
+
+player_y = 500
+player_x = 500
+player = canvas.create_rectangle(player_x, player_y, player_x+50, player_y+50, fill="red")
+block_x = 500
+block_y = 470
+block = canvas.create_rectangle(block_x,block_y,block_x+50,block_y-50,fill="green")
+
+def create_block():
+    global block_x
+    global player_x
+    if block_y >= player_y:
+        if block_x >= player_x and block_x + 50 <= player_x +50:
+            create_coin()
+
+
+
+def create_coin():
+    coin = canvas.create_oval(500, 420, 550, 370, fill="yellow")
+
 win.mainloop()
